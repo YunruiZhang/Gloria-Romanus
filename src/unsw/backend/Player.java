777bcbcb10d4aa58715300b9c6c89669c74e4760
Edward <yunrui.zhang@student.unsw.edu.au>
2020-10-22 16:42:43 +1100

    first

---
+++ src/unsw/backend/Player.java
@@ -0,0 +1,15 @@
+package unsw.backend;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class Player {
+    private  ArrayList<Province> provinces;
+    private int gold;
+    private int total_wealth;
+    private boolean conquest_goal;
+    private boolean treasury_goal;
+    private boolean infustructure_goal;
+}
