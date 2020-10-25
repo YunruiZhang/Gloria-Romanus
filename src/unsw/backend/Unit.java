067b3ebed98deb3e667b252e974e3d52ccb26144
Edward <yunrui.zhang@student.unsw.edu.au>
2020-10-25 13:17:51 +1100

    add bonus

--- src/unsw/backend/Unit.java
+++ src/unsw/backend/Unit.java
@@ -5,6 +5,10 @@ abstract public class Unit {
     private String name;
     private Province location;
     private String type;
+    private double reduceEnemyDamage;
+    private double soilderSpeed;
+    private double reduceEnemyDamagePercent;
+
     public abstract void move(Province dest);
     public abstract void attact();
     public Unit(int soldiers, String name, Province location, String type){
@@ -12,6 +16,8 @@ abstract public class Unit {
         this.name = name;
         this.location = location;
         this.type = type;
+        this.reduceEnemyDamage = 0;
+        this.soilderSpeed = 1;
     }
     public String getName(){
         return name;
@@ -21,5 +27,14 @@ abstract public class Unit {
     }
     public void soliders_die(int num){
         soldiers -= num;
+    }
+
+    public double caculateEnemyDamage(double EDamage){
+        double damageLeft = (EDamage * (1-reduceEnemyDamagePercent)) - reduceEnemyDamage;
+        if(damageLeft < 1){
+            return 1;
+        }else{
+            return damageLeft;
+        }
     }
 }
