b655f920fafc9b593fede01b23c4db1d6403ddae
Arpit <a.rulania@student.unsw.edu.au>
2020-10-28 01:02:25 +1100

    coded all the special abilites of infra

--- src/unsw/backend/Wall.java
+++ src/unsw/backend/Wall.java
@@ -1,8 +1,16 @@
 package unsw.backend;
 
-public class Wall {
+public class Wall extends Infrastructure{
     //these provide buffers to all troops defending a province. 
     //They have a random chance of causing damage to attacking troops 
     //if upgraded to have archer towers or ballista towers. 
     //Towers have infinite morale, and can only be damaged by artillery (but are instantly repaired without cost after the battle).
+    static private String type = "Wall";
+    static private int BuildTime = 4;
+    static private int maxUpgrade = 3;
+
+    public Wall (Province province) {
+        super(BuildTime, maxUpgrade, type, province);
+    }
+
 }
