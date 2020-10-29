3cff536ff35cf0ed572076fecbbad98262d9b06c
Edward <yunrui.zhang@student.unsw.edu.au>
2020-10-29 19:29:41 +1100

    fix merge

--- src/unsw/backend/Artillery.java
+++ src/unsw/backend/Artillery.java
@@ -1,12 +1,10 @@
 package unsw.backend;
 
 public class Artillery extends Unit implements Observer{
-    final static double killDamage = 100;
-    final static double defencePoint = 80;
     final static String className = "Artillery";
 
     public Artillery(String name, Province location, String type) {
-        super(name, location, type, killDamage, defencePoint, className, 4);
+        super(name, location, type, className, 4);
     }
     public void update(Object o){
         super.setMovePoint(4);
--- src/unsw/backend/Cavalry.java
+++ src/unsw/backend/Cavalry.java
@@ -2,12 +2,10 @@ package unsw.backend;
 
 
 public class Cavalry extends Unit implements Observer{
-    final static double killDamage = 60;
-    final static double defencePoint = 50;
     final static String className = "Cavalry";
 
     public Cavalry(String name, Province location, String type) {
-        super(name, location, type, killDamage, defencePoint, className, 15);
+        super(name, location, type, className, 15);
     }
 
     public void update(Object o){
