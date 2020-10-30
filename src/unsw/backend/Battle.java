a35af2f9ef881130fb48e4c67e301825c46e52c2
Arpit <a.rulania@student.unsw.edu.au>
2020-10-30 17:21:35 +1100

    no errors left in the battle need to do flea now

--- src/unsw/backend/Battle.java
+++ src/unsw/backend/Battle.java
@@ -16,7 +16,7 @@ public class Battle {
         this.engagementCounter = 0;
     }
 
-    public boolean StartBattle() {
+    public int StartBattle() {
         while (MyArmy.size() > 0 && OppositionArmy.size() > 0 && !draw) {
             int myIndex = ThreadLocalRandom.current().nextInt(0, MyArmy.size() + 1);
             int oppositionIndex = ThreadLocalRandom.current().nextInt(0, OppositionArmy.size() + 1);
@@ -24,6 +24,9 @@ public class Battle {
             Unit enemy = OppositionArmy.get(oppositionIndex);
             startSkirmish(friend, enemy);
         }
+        if (draw || (MyArmy.size() <= 0 && OppositionArmy.size() <= 0)) return 0;   //nobody won
+        if (MyArmy.size() <= 0) return 2;   //opposition won
+        return 1; //We won!
     }
 
     public void startSkirmish(Unit friend, Unit enemy) {
@@ -87,14 +90,42 @@ public class Battle {
     }
 
     public boolean getDestroyedLongRange(Unit friend, Unit enemy) {
+        Random r = new Random();
         if (friend.getMelee()) {
-            double damage  = (friend.getSoldiers() * 0.1)*(discountedDamage(friend, enemy)/(friend.GetArmour() + friend.GetShield()));
+            double damage  = (friend.getSoldiers() * 0.1)*(discountedDamage(friend, enemy)/(friend.GetArmour() + friend.GetShield()))*(1 + r.nextGaussian());
+            friend.soliders_die((int)damage);
         } else {
-            double damagee  = (enemy.getSoldiers() * 0.1)*(discountedDamage(enemy, friend)/(enemy.GetArmour() + enemy.GetShield()));
+            double damagee  = (enemy.getSoldiers() * 0.1)*(discountedDamage(enemy, friend)/(enemy.GetArmour() + enemy.GetShield()))*(1 + r.nextGaussian());
+            enemy.soliders_die((int)damagee);
         }
+        if (friend.getSoldiers() <= 0 || enemy.getSoldiers() <= 0) {
+            removeFromArmy(friend, enemy);
+            return false;
+        } else return true;
     }
 
     public boolean getDestroyedShortRange(Unit friend, Unit enemy) {
+        Random r = new Random();
+        double damA = (friend.getSoldiers() * 0.1)*(discountedDamage(friend, enemy)/(friend.GetArmour() + friend.GetShield() + enemy.getCSkill()))*(1 + r.nextGaussian());
+        double damB = (enemy.getSoldiers() * 0.1)*(discountedDamage(enemy, friend)/(enemy.GetArmour() + enemy.GetShield() + enemy.getCSkill()))*(1 + r.nextGaussian());
+        friend.soliders_die((int)damA);
+        friend.soliders_die((int)damB);
+        if (friend.getSoldiers() <= 0 || enemy.getSoldiers() <= 0) {
+            removeFromArmy(friend, enemy);
+            return false;
+        } else return true;
+    }
+
+    public void removeFromArmy(Unit friend, Unit enemy) {
+        if (friend.getSoldiers() <= 0) {
+            MyArmy.remove(friend);
+        }
+        if (enemy.getSoldiers() <= 0) {
+            OppositionArmy.remove(enemy);
+        }
+    }
+
+    public void UnitAbort(Unit coward) {
 
     }
 }
