27a00236a45958bfe78252b14fcce7242128ad12
Arpit <a.rulania@student.unsw.edu.au>
2020-10-30 18:03:50 +1100

    finished flea calculator

--- src/unsw/backend/Battle.java
+++ src/unsw/backend/Battle.java
@@ -9,6 +9,10 @@ public class Battle {
     private ArrayList<Unit> OppositionArmy;
     private int engagementCounter;
     private boolean draw = false;
+    private int mysize;
+    private int enemysize;
+    private int myloss;
+    private int enemyloss;
 
     public Battle(ArrayList<Unit> MyArmy, ArrayList<Unit> OppositionArmy) {
         this.MyArmy = MyArmy;
@@ -63,15 +67,29 @@ public class Battle {
     }
 
     public boolean startEngagement(boolean range, Unit friend, Unit enemy){
+        mysize = friend.getSoldiers();
+        enemysize = enemy.getSoldiers();
         if (engagementCounter + 1 > 200) {
             draw = true;
             return false;
         }
+        boolean ashesh;
         if (range) {
-            return getDestroyedLongRange(friend, enemy);
+            ashesh = getDestroyedLongRange(friend, enemy);
         } else {
-            return getDestroyedShortRange(friend, enemy);
+            ashesh = getDestroyedShortRange(friend, enemy);
         }
+        if (!ashesh) {
+            if (MyArmy.contains(friend)) {
+                FleaCalculator(friend, null);
+            } else {
+                //flee shit enemy
+                FleaCalculator(null, enemy);
+            }
+        } else { 
+            FleaCalculator(friend, enemy);
+        }
+        return ashesh;
     }
 
     public boolean EngagementChanceCalculator(Unit friend, Unit enemy) {
@@ -94,9 +112,13 @@ public class Battle {
         if (friend.getMelee()) {
             double damage  = (friend.getSoldiers() * 0.1)*(discountedDamage(friend, enemy)/(friend.GetArmour() + friend.GetShield()))*(1 + r.nextGaussian());
             friend.soliders_die((int)damage);
+            myloss = (int)damage;
+            enemyloss = 0;
         } else {
             double damagee  = (enemy.getSoldiers() * 0.1)*(discountedDamage(enemy, friend)/(enemy.GetArmour() + enemy.GetShield()))*(1 + r.nextGaussian());
             enemy.soliders_die((int)damagee);
+            enemyloss = (int)damagee;
+            myloss = 0;
         }
         if (friend.getSoldiers() <= 0 || enemy.getSoldiers() <= 0) {
             removeFromArmy(friend, enemy);
@@ -106,10 +128,13 @@ public class Battle {
 
     public boolean getDestroyedShortRange(Unit friend, Unit enemy) {
         Random r = new Random();
+        
         double damA = (friend.getSoldiers() * 0.1)*(discountedDamage(friend, enemy)/(friend.GetArmour() + friend.GetShield() + enemy.getCSkill()))*(1 + r.nextGaussian());
         double damB = (enemy.getSoldiers() * 0.1)*(discountedDamage(enemy, friend)/(enemy.GetArmour() + enemy.GetShield() + enemy.getCSkill()))*(1 + r.nextGaussian());
         friend.soliders_die((int)damA);
-        friend.soliders_die((int)damB);
+        enemy.soliders_die((int)damB);
+        myloss = (int)damA;
+        enemyloss = (int)damB;
         if (friend.getSoldiers() <= 0 || enemy.getSoldiers() <= 0) {
             removeFromArmy(friend, enemy);
             return false;
@@ -123,6 +148,22 @@ public class Battle {
         if (enemy.getSoldiers() <= 0) {
             OppositionArmy.remove(enemy);
         }
+    }
+
+    public void FleaCalculator(Unit friend, Unit enemy) {
+        double friendFleaBase = -1;
+        double enemyFleaBase = -1;
+        if (friend != null) {
+            if ((1 - (friend.getMorale() * 0.1)) < 0.05) friendFleaBase = 0.05;
+            else friendFleaBase = 1 - (friend.getMorale() * 0.1);
+        }
+        if (enemy != null) {
+            if ((1 - (enemy.getMorale() * 0.1)) < 0.05) enemyFleaBase = 0.05;
+            else enemyFleaBase = 1 - (enemy.getMorale() * 0.1);
+        }
+        if (friendFleaBase != -1) friendFleaBase += (((myloss/mysize)/(enemyloss/enemysize))*(0.1));
+        if (enemyFleaBase != -1) enemyFleaBase += (((enemyloss/enemysize)/(myloss/mysize))*(0.1));
+
     }
 
     public void UnitAbort(Unit coward) {
