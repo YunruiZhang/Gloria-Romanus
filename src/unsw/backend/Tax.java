96d07709dae36c85d0104ff5050fdfa9dec1dce8
Arpit <a.rulania@student.unsw.edu.au>
2020-11-01 15:10:57 +1100

    the moving stuff to tax.java worked very well

--- src/unsw/backend/Tax.java
+++ src/unsw/backend/Tax.java
@@ -12,7 +12,7 @@ public class Tax {
     /**
      * changes the wealth every turn according to the tax rate.
      */
-    public void calculateWealth() {
+    public void CalculateWealth() {
         if (taxRate == 10) {
             province.provinceWealthAdder(10);
         } else if (taxRate == 15) {
@@ -30,6 +30,58 @@ public class Tax {
                 province.provinceWealthAdder(-30);
             }
             province.decreaseAllMorale();
+        }
+    }
+
+    /**
+     * @return double return the taxRate
+     */
+    public double GetTaxRate() {
+        return taxRate;
+    }
+
+    /**
+     * the taxRate to set
+     */
+    public void IncreaseTaxRate() {
+        if (taxRate < 25) {
+            taxRate += 5;
+        } else {
+            //System.out.println("tax rate has reached its limit");
+        }
+    }
+
+    /**
+     * the taxRate to set
+     */
+    public void DecreaseTaxRate() {
+        if (taxRate > 10) {
+            taxRate -= 5;
+        } else {
+            //System.out.println("tax rate is already minimum");
+        }
+    }
+
+    /**
+     * @param level sets a particular tax rate given by the user.
+     */
+    public void SetTax(int level){
+        switch(level){
+            case 1:
+                taxRate = 10;
+                break;
+            case 2:
+                taxRate = 15;
+                break;
+            case 3: 
+                taxRate = 20;
+                break;
+            case 4:
+                taxRate = 25;
+                break;
+            default:
+                //System.out.println("WrongNumber"); //JAVAFX ~~~~~~~~~~~
+                break;
         }
     }
     
