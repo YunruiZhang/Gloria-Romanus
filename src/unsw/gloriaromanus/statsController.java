56703388cb5097c39e805ebfbd378bc7ab7abef6
Arpit Rulania <arpitunsw@gmail.com>
2020-11-15 13:04:07 +1100

    1 to 2

--- src/unsw/gloriaromanus/statsController.java
+++ src/unsw/gloriaromanus/statsController.java
@@ -5,6 +5,6 @@ import javafx.event.ActionEvent;
 public class statsController extends MenuController{
     public void updateGoldAmt(ActionEvent e) throws Exception {
         getParent().getGoldAmount(e, 1);
-        getParent().getGoldAmount(e, 1);
+        getParent().getGoldAmount(e, 2);
     }
 }
