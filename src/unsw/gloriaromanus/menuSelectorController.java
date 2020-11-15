9874b600900fa453b22a5149f15ae6df3f128aad
Arpit Rulania <arpitunsw@gmail.com>
2020-11-15 16:12:49 +1100

    formatting and fonts

--- src/unsw/gloriaromanus/menuSelectorController.java
+++ src/unsw/gloriaromanus/menuSelectorController.java
@@ -1,5 +1,11 @@
 package unsw.gloriaromanus;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class menuSelectorController extends MenuController{
-    
+    @FXML
+    public void menumenunecxtturn(ActionEvent e) throws IOException {
+        getParent().nextTurnClick(e);
+    }
 }
